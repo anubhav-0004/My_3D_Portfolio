b1da0087a23e8e30afbed0a24df9a67003d3369b
anubhav-0004 <2003anubhav@gmail.com>
2025-01-23 15:59:52 +0530

    .

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -11,7 +11,7 @@ const Tech = () => {
 
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
+      {displayedTechnologies.map((technology) => (
         <div
           className="w-28 h-28"
           key={technology.name}
